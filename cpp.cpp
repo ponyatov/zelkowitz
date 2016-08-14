2643c52c61b4d813679d5380cf3aa62336b20da3
Dmitry Ponyatov <dponyatov@gmail.com>
2016-08-14 18:00:17 +0400

    + typical infix parser
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ cpp.cpp
@@ -0,0 +1,14 @@
+#include "hpp.hpp"
+#define YYERR "\n\n"<<yylineno<<":"<<msg<<"["<<yytext<<"]\n\n"
+void yyerror(string msg) { cout<<YYERR; cerr<<YYERR; exit(-1); }
+int main() { return yyparse(); }
+
+Sym::Sym(string V) { val=V; }
+void Sym::push(Sym*o) { nest.push_back(o); }
+
+string Sym::head() { return "<"+val+">"; }
+string Sym::pad(int n) { string S; for (int i=0;i<n;i++) S+='\t'; return S; }
+string Sym::dump(int depth) { string S = "\n"+pad(depth)+head();
+	for (auto it=nest.begin(),e=nest.end();it!=e;it++)
+		S += (*it)->dump(depth+1);
+	return S; }
---
+++ hpp.hpp
@@ -0,0 +1,25 @@
+#ifndef _H_HPP
+#define _H_HPP
+
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <map>
+using namespace std;
+
+struct Sym {
+	string val;
+	Sym(string);
+	vector<Sym*> nest; void push(Sym*);
+	virtual string dump(int=0); virtual string head(); string pad(int);
+};
+
+extern int yylex();
+extern int yylineno;
+extern char* yytext;
+#define TOC(C,X) { yylval.o = new C(yytext); return X; }
+extern int yyparse();
+extern void yyerror(string);
+#include "ypp.tab.hpp"
+
+#endif // _H_HPP
